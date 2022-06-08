9432c9a6086163a3bdbbf358f94010208749c66f
Saad Siddiq <siddiq.saad@gmail.com>
2022-06-08 17:46:46 -0500

    added stdin for input

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require('./client');
 
+// setup interface to handle user input from stdin
+const setupInput = function(cb) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", data => cb(data));
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // Ctrl+C to exit
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput(handleUserInput);
 console.log("Connecting ...");
 connect();
